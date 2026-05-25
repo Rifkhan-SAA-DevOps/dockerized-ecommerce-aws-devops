9e2cd4748a1ec5f7c314f762f28382911cd7d94c
Rifkhankhan <rifkhanmuhammed17@gmail.com>
2026-05-25 15:33:38 +0530

    Docker Image Optimization

--- server/src/app.js
+++ server/src/app.js
@@ -15,11 +15,18 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+//   }),
+// );
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: ["http://localhost:3000", "http://localhost:5173"],
     credentials: true,
-  })
+  }),
 );
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
@@ -30,7 +37,7 @@ app.use(
     max: 300,
     standardHeaders: true,
     legacyHeaders: false,
-  })
+  }),
 );
 
 app.get("/api/health", (req, res) => {
