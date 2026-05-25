952c0f3a1a44474ac22aea3341aaca802ab427e9
Rifkhankhan <rifkhanmuhammed17@gmail.com>
2026-05-25 17:41:32 +0530

    Add Docker restart policy and log rotation

--- server/src/app.js
+++ server/src/app.js
@@ -15,19 +15,19 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     credentials: true,
-//   }),
-// );
-
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "http://localhost:5173"],
+//     credentials: true,
+//   }),
+// );
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
